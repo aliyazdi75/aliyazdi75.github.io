a4f088e45c83789f2acacc73c4ccd2020982037e
aliyazdi75 <ma.yazdi75@gmail.com>
2020-02-22 01:45:46 +0330

    add font icons for socials, add system bottom navigation color customized, add medium size to adaptive, some ui improvement and design

---
+++ lib/data/icons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:my_site/core/constants/index.dart';
+
+class MySiteIcons {
+  MySiteIcons._();
+
+  static const IconData email = IconData(
+    0xe900,
+    fontFamily: MySiteIconsFont,
+  );
+  static const IconData resume = IconData(
+    0xe901,
+    fontFamily: MySiteIconsFont,
+  );
+  static const IconData twitter = IconData(
+    0xe902,
+    fontFamily: MySiteIconsFont,
+  );
+  static const IconData github = IconData(
+    0xe903,
+    fontFamily: MySiteIconsFont,
+  );
+  static const IconData linkedin = IconData(
+    0xe904,
+    fontFamily: MySiteIconsFont,
+  );
+}
---
+++ lib/screens/home/components/src/avatar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:my_site/core/constants/index.dart';
+
+class Avatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final shortestSide = MediaQuery.of(context).size.shortestSide;
+
+    return CircleAvatar(
+      radius: shortestSide / 5.0,
+      backgroundImage: AssetImage(avatarPath),
+    );
+  }
+}
